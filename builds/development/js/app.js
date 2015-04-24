2cef68c42b5e9ceecb350de8ccec1e1b5d607227
vamshins <Nsvk001$>
2015-04-24 17:28:40 -0600

    first commit

---
+++ builds/development/js/app.js
@@ -0,0 +1,21 @@
+var myApp = angular.module('myApp', ['ngRoute', 'appControllers']);
+
+var appControllers = angular.module('appControllers', []);
+
+myApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.
+		when('/login', {
+			templateUrl: 'views/login.html',
+			controller: 'RegistrationController'
+		}).
+		when('/register', {
+			templateUrl: 'views/register.html',
+			controller: 'RegistrationController'
+		}).
+		when('/meetings', {
+			templateUrl: 'views/meetings.html'
+		}).
+		otherwise({
+			redirectTo: '/login'
+		});
+}]);
---
+++ builds/development/js/controllers/registration.js
@@ -0,0 +1,7 @@
+myApp.controller('RegistrationController', function($scope, $location) {
+	
+	$scope.login = function() {
+		$location.path('/meetings')
+	} //login
+	
+}); // RegistrationController
